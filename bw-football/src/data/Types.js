6d198410d68f314acd24f822b6ec9405ddce8a33
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2020-01-10 16:57:01 +0100

    Creating the redux data stpre
    Actions are objects that are sent to the data store to make changes to the data it contains. Actions have
    types (found in \data\Types.js), and action objects are created using action creators (found in \data\ActionCreators.js)

---
+++ bw-football/src/data/Types.js
@@ -0,0 +1,7 @@
+expost const DataTypes = {
+	COMPETITIONS: "competitions"
+}
+
+export const ActionTypes = {
+	DATA_LOAD: "data_load"
+}
---
+++ bw-football/src/data/placeholderData.js
@@ -0,0 +1,11 @@
+export const data = {
+	competitions: [
+		{id: 2014, name: "La Liga"},
+		{id: 2021, name: "Premier League"},
+		{id: 2001, name: "UEFA"},
+		{id: 2002, name: "Bundesliga"},
+		{id: 2015, name: "Ligue 1"},
+		{id: 2017, name: "Primeira Liga"},
+		{id: 2146, name: "UEFA Europa League"}
+	]
+}
